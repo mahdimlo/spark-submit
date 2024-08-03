fc11ab08952fc76e70d346a2456da68df385df0d
Mahdi <155837868+mahdimlo@users.noreply.github.com>
2024-08-03 16:00:46 +0330

    latest

--- namenode/init-hdfs.sh
+++ namenode/init-hdfs.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
-sleep 30
+# Function to check HDFS availability
+check_hdfs() {
+  hdfs dfs -ls / > /dev/null 2>&1
+  return $?
+}
 
+# Retry logic
+retries=30
+until check_hdfs; do
+  if [ $retries -le 0 ]; then
+    echo "HDFS service not available after multiple retries."
+    exit 1
+  fi
+  echo "Waiting for HDFS service..."
+  sleep 10
+  retries=$((retries - 1))
+done
+
+# Create a directory in HDFS
 hdfs dfs -mkdir -p /user/history
 
+# Put the local file into HDFS
 hdfs dfs -put /tmp/history.txt /user/history
